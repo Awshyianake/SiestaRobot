8e98233f6ef0fbba186433c1c208fddbd1e1ce37
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-09-20 02:35:47 +0700

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,16 +9,16 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/c880f57deef159e1e9b6c.jpg"
+PHOTO = "https://telegra.ph//file/ae55c07c1e92161158d55.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ɴɪsᴋᴀʟᴀ.** \n"
+  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ꜰᴜᴛᴧɴōᴋɪ.** \n"
   TEXT += "**ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ** \n\n"
-  TEXT += f"💢 **ᴍʏ ᴍᴀsᴛᴇʀ : [ᴀʟ](https://t.me/IDnyaAL)** \n"
-  TEXT += f"💢 **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n"
-  TEXT += f"💢 **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n"
-  TEXT += f"💢 **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += "**ᴛʜᴀɴᴋs ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ 🥰**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/NiskalaXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/NiskalaSupport")]]
+  TEXT += f"♡ **ᴍʏ ᴍᴀsᴛᴇʀ : [sʜɪɴᴢō](https://t.me/ShinzoShitsuren)** \n"
+  TEXT += f"♡ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n"
+  TEXT += f"♡ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n"
+  TEXT += f"♡ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
+  TEXT += "**ᴛʜᴀɴᴋs ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ 😉**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/FutanokiRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/stufsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
