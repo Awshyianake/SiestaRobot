88c3d8b62ba2ee28bea92b258453b52b0532a110
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-09-19 22:10:45 +0700

    Update afk.py

--- SiestaRobot/modules/afk.py
+++ SiestaRobot/modules/afk.py
@@ -160,7 +160,10 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "Afk​"
+def helps(chat):
+    return gs(chat, "afk_help")
+
+__mod_name__ = "ᴀꜰᴋ"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
