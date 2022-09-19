94623c58c0c7e8106732afe1c4b7e55b57296bd7
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-09-20 06:00:23 +0700

    Update chatbot.py

--- SiestaRobot/modules/chatbot.py
+++ SiestaRobot/modules/chatbot.py
@@ -144,7 +144,7 @@ def helps(chat):
 
 __mod_name__ = "ᴄʜᴀᴛʙᴏᴛ"
 
-CHATBOTK_HANDLER = CommandHandler("chatbot", Anoki )
+CHATBOTK_HANDLER = CommandHandler("chatbot", anoki )
 ADD_CHAT_HANDLER = CallbackQueryHandler(kukiadd, pattern=r"add_chat" )
 RM_CHAT_HANDLER = CallbackQueryHandler(kukirm, pattern=r"rm_chat" )
 CHATBOT_HANDLER = MessageHandler(
