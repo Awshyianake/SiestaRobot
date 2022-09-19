fa4c57d596e6b15b793f36e6974fb351fb8847d5
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-09-19 19:49:53 +0700

    Update gtranslator.py

--- SiestaRobot/modules/gtranslator.py
+++ SiestaRobot/modules/gtranslator.py
@@ -18,7 +18,7 @@ from SiestaRobot.modules.language import gs
 def helps(chat):
     return gs(chat, "translator_help")
 
-__mod_name__ = "Tʀᴀɴsʟᴀᴛᴏʀ"
+__mod_name__ = "ᴛʀᴀɴsʟᴀᴛᴏʀ"
 
 
 trans = Translator()
