e6324803bb9bf35132d9641a764e76b078691e3d
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-09-20 02:46:07 +0700

    Update donate.py

--- SiestaRobot/modules/donate.py
+++ SiestaRobot/modules/donate.py
@@ -6,10 +6,10 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/c880f57deef159e1e9b6c.jpg"
+PHOTO = "https://telegra.ph//file/ae55c07c1e92161158d55.jpg"
 
 @register(pattern=("/donate"))
 async def awake(event):
   TEXT = f"ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏɴᴀᴛᴇ ᴛᴏ ɴɪsᴋᴀʟᴀ ʀᴏʙᴏᴛ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ʜᴇʟᴘ sᴜᴘᴘᴏʀᴛ ᴜs sᴏ ᴛʜᴀᴛ ʙᴏᴛs sᴛᴀʏ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ʜᴇʟᴘ ʏᴏᴜʀ ᴀᴄᴛɪᴠɪᴛɪᴇs ᴏɴ ᴛᴇʟᴇɢʀᴀᴍ 😉"
-  BUTTON = [[Button.url("💢 ᴅᴏɴᴀᴛᴇ 💢", "https://t.me/+eOyeaWYq4BUyZjhl")]]
+  BUTTON = [[Button.url("ᴅᴏɴᴀᴛᴇ 🎁", "https://t.me/ShinzoShitsuren")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
