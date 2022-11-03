cfe8c5f1f158c2f9a25ef4d72d393c42701827e5
Awshyianake <113591693+Awshyianake@users.noreply.github.com>
2022-11-03 13:00:09 +0700

    Update chatbot.py

--- SiestaRobot/modules/chatbot.py
+++ SiestaRobot/modules/chatbot.py
@@ -118,7 +118,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         sweetie = message.text
         bot.send_chat_action(chat_id, action="typing")
-        url = f"https://kukiapi.xyz/api/apikey=1356469075-KUKIkq4WMg5FV4/NISKALA/RZYDX/message={sweetie}" 
+        url = f"https://api.roseloverx.com/api/chatbot?message=Hi+There" 
         request = requests.get(url) 
         results = json.loads(request.text) 
         boyresult = f"{results['reply']}"
